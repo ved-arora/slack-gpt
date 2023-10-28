b369a4761c2f29e993444258f6ac4f4061780434
ved-arora <vxa162@case.edu>
2023-10-28 16:32:56 -0400

    added functions to clean the message history

--- slack-gpt-python.py
+++ slack-gpt-python.py
@@ -5,6 +5,9 @@ from langchain.llms import OpenAI
 from langchain.chains import ConversationChain, LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+import datetime
+from slack_sdk import WebClient
+import re
 
 # Initialize app with bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
@@ -34,15 +37,65 @@ chatgpt_chain = LLMChain(
     memory=ConversationBufferWindowMemory(k=2),
 )
 
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+def get_username_from_id(user_id):
+    response = client.users_info(user=user_id)
+    return response['user']['name']
+
+def replace_tags(messages):
+    # Define the regex pattern to match patterns like <@U063XFA8JAC>
+    pattern = re.compile(r'<@([^>]+)>')
+    
+    # Use a list comprehension to replace each occurrence in every message
+    messages = [pattern.sub(r'\1', message) for message in messages]
+    return messages
+
+def cleanMessage(messages):
+    return [message for message in messages if not message[0].startswith(' ')]
+
+
+def fetch_messages_from_last_24_hours(channel_id, token):
+    # Calculate the timestamp for 24 hours ago
+    one_day_ago = datetime.datetime.now() - datetime.timedelta(days=1)
+    # Convert to Slack's timestamp format
+    oldest_timestamp = one_day_ago.timestamp()
+
+    # Initialize a Slack client
+    client = WebClient(token=token)
+
+    # Fetch the channel's history
+    response = client.conversations_history(
+        channel=channel_id,
+        oldest=oldest_timestamp
+    )
+    messages = [message['text'] for message in response['messages']]
+    named_messages = replace_tags(messages)
+    # Return the messages
+    return named_messages
 
 @app.event("app_mention")
-def handle_app_mention_events(body, say, logger):
+def handle_app_mention_events(body, logger, client):
     logger.info(body)
-    # Extract the text from the event body
+    # Extract the text and user from the event body
     text = body['event']['text']
+    user_id = body['event']['user']
+    channel_id = body['event']['channel']
+    
     # Respond to the mention (for example, you could use your chat model here)
     output = chatgpt_chain.predict(human_input=text)
-    say(output)
+    
+    # Send an ephemeral message to the user who mentioned the app
+    client.chat_postEphemeral(
+        channel=channel_id,
+        user=user_id,
+        text=output
+    )
+    
+    # Fetch the last 24 hours of messages (if you still need this functionality)
+    messages = cleanMessage(fetch_messages_from_last_24_hours(channel_id, os.environ.get("SLACK_BOT_TOKEN")))
+    print(messages)
+
 
 
 # Start the app
